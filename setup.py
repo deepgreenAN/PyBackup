22c8445212500981325e28c397a79594880ab56d
deepgreenAN <asami73dgreen63@gmail.com>
2021-03-31 10:09:58 +0900

    ディレクトリ構成を変更

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+install_requires = [
+    "schedule"
+]
+
+packages = [
+    "py_backup",
+    "py_backup_cli"
+]
+
+console_scripts = [
+    'schedule_and_backup=py_backup_cli.schedule_and_backup:main',
+]
+
+
+setup(
+    name='py_backup',
+    version='0.0.0',
+    packages=packages,
+    install_requires=install_requires,
+    entry_points={'console_scripts': console_scripts},
+)
